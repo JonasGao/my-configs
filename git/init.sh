b3a8b4beaef9ddf953d03ff9e96026ecc988789b
Jonas <jonas.gao@voyageone.cn>
2019-08-12 16:59:49 +0800

    add git-push-create-branch

--- git/init.sh
+++ git/init.sh
@@ -13,8 +13,6 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-git config --global alias.curr-br git symbolic-ref --short HEAD
-git config --global alias.push-new push origin $(git curr-br)
 echo "Setup completion"
 
 ## editor settings
