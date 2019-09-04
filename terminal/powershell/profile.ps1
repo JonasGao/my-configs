d06110f5518ce2378c325561d155d0b7010d79c5
Jonas <jonas.gao@voyageone.cn>
2019-09-04 11:59:42 +0800

    Update profile.ps1

--- terminal/powershell/profile.ps1
+++ terminal/powershell/profile.ps1
@@ -1,2 +1,10 @@
+New-Alias p pnpm
+New-Alias y yarn
 New-Alias ll ls
+
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+Import-Module posh-git
+Import-Module oh-my-posh
 Import-Module posh-gvm
+Set-Theme Agnoster
