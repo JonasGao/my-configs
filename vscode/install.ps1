f57a20eeb1d874ca8bc0fa25555105163fff72eb
Jonas <jonas.gao@voyageone.cn>
2019-09-06 11:08:57 +0800

    Update install.ps1

--- vscode/install.ps1
+++ vscode/install.ps1
@@ -1,8 +1,13 @@
 param (
-  [String] $Target
+  [String] $Target,
+  [String] $Proxy
 )
 
-Invoke-RestMethod https://update.code.visualstudio.com/latest/win32-x64-archive/stable -OutFile vscode.zip
+if ($Proxy) {
+  Invoke-RestMethod https://update.code.visualstudio.com/latest/win32-x64-archive/stable -OutFile vscode.zip -Proxy $Proxy
+} else {
+  Invoke-RestMethod https://update.code.visualstudio.com/latest/win32-x64-archive/stable -OutFile vscode.zip
+}
 Expand-Archive ./vscode.zip -DestinationPath $Target
 Remove-Item vscode.zip
 New-Item -Path $Target/data -ItemType "directory"
