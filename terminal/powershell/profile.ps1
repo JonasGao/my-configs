a325ba3c24c694b0794b982d0d82c24d47d84028
Jonas Gao <gaopinsong@live.cn>
2019-11-07 13:53:02 +0800

    Update profile.ps1

--- terminal/powershell/profile.ps1
+++ terminal/powershell/profile.ps1
@@ -7,6 +7,54 @@
 # if (!(Test-Path -Path $PROFILE )) 
 # { New-Item -Type File -Path $PROFILE -Force }
 
+function Copy-ItemUsingExplorer {
+  param(
+    [string]$Source,
+    [string]$Destination,
+    [int]$CopyFlags = 0
+  )
+
+  if (!$Source) {
+    Write-Output "There is no source"
+    return 1
+  }
+
+  if (!$Destination) {
+    Write-Output "There is no destination"
+    return 2
+  }
+
+  $objShell = New-Object -ComObject 'Shell.Application'    
+  $objFolder = $objShell.NameSpace((Get-Item $Destination).FullName)
+  $objFolder.CopyHere((Get-Item $Source).FullName, $CopyFlags.ToString('{0:x}'))
+}
+
+function Move-ItemUsingExplorer {
+  param(
+    [string]$Source,
+    [string]$Destination,
+    [int]$MoveFlags = 0
+  )
+
+  if (!$Source) {
+    Write-Output "There is no source"
+    return 1
+  }
+
+  if (!$Destination) {
+    Write-Output "There is no destination"
+    return 2
+  }
+
+  $objShell = New-Object -ComObject 'Shell.Application'    
+  $objFolder = $objShell.NameSpace((Get-Item $Destination).FullName)
+  $objFolder.MoveHere((Get-Item $Source).FullName, $MoveFlags.ToString('{0:x}'))
+}
+
+New-Alias emv Move-ItemUsingExplorer
+New-Alias ecp Copy-ItemUsingExplorer
+New-Alias which Get-Command
+New-Alias grep findstr
 New-Alias p pnpm
 New-Alias y yarn
 New-Alias ll ls
