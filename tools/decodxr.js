9341580edf2ee5970df8456f3f5db4af434d7b66
Jonas Gao <gaopinsong@live.cn>
2019-11-24 14:50:46 +0800

    Update decodxr.js

--- tools/decodxr.js
+++ tools/decodxr.js
@@ -1,55 +1,54 @@
-
 function decode(url) {
-
-  const removePrefix = (url) => {
+  const removePrefix = url => {
     if (url.indexOf("ssr://") == 0) {
-      return url.substring(6)
+      return url.substring(6);
     }
-    return url
-  }
+    return url;
+  };
 
-  const decodeBase64 = (data) => {
-    const buff = Buffer.from(data, 'base64')
-    return buff.toString('ascii')
-  }
+  const decodeBase64 = data => {
+    const buff = Buffer.from(data, "base64");
+    return buff.toString("ascii");
+  };
 
-  const decodeUrl = (url) => {
+  const decodeMainPart = (url, opt) => {
+    let main = url;
     if (url.indexOf("/?") >= 0) {
-      const arr = url.split("/?")
-      const part1 = arr[0].split(":")
-      const part2 = arr[1].split("&").reduce((o, s) => {
-        let [key, value] = s.split("=")
-        if (value) {
-          value = decodeBase64(value)
-        }
-        o[key] = value
-        return o
-      }, {})
-      const opt = {
-        ss: {
-          host: part1[0],
-          port: part1[1],
-          cipher: part1[3],
-          password: part1[5]
-        },
-        obfs:part1[4],
-        obfsParam:part2['obfsparam'],
-        protocol: part1[2],
-        protocolParam: part2['protoparam'],
-        group: part2['group']
-      }
-      return opt
+      main = url.substring(0, url.indexOf("/?"));
     }
-    return url
-  }
-  
-  return decodeUrl(
-    decodeBase64(
-      removePrefix(url)
-    )
-  )
+    const values = main.split(":");
+    return Object.assign(opt, {
+      host: values[0],
+      port: values[1],
+      cipher: values[3],
+      password: values[5],
+      obfs: values[4],
+      protocol: values[2]
+    });
+  };
+
+  const decodeParams = (url, opt) => {
+    if (url.indexOf("/?") < 0) {
+      return opt;
+    }
+    const params = url.substring(url.indexOf("/?") + 2);
+    return params.split("&").reduce((o, s) => {
+      let [key, value] = s.split("=");
+      if (value) {
+        value = decodeBase64(value);
+      }
+      o[key] = value;
+      return o;
+    }, opt);
+  };
+
+  const decodeUrl = url => {
+    return decodeParams(url, decodeMainPart(url, {}));
+  };
+
+  return decodeUrl(decodeBase64(removePrefix(url)));
 }
 
-const [, , input] = process.argv
+const [, , input] = process.argv;
 
-console.log(decode(input))
+console.log(decode(input));
