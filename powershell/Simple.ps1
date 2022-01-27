44424378228490c19e1d90b3fbf840de5f7d7147
Jonas Gao <gaopinsong@live.cn>
2022-01-27 16:25:15 +0800

    Microsoft.PowerShell_profile.ps1

--- powershell/Simple.ps1
+++ powershell/Simple.ps1
@@ -1,7 +1,8 @@
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Import-Module posh-git
-
+Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
 Import-Module ZLocation
+Import-Module posh-git
+Import-Module Terminal-Icons
 
 if (Test-Path "C:\Users\Administrator\.jabba\jabba.ps1") { . "C:\Users\Administrator\.jabba\jabba.ps1" }
 
@@ -40,6 +41,23 @@ function Set-HttpProxy ([string]$Url, [switch]$Reset) {
   $Env:HTTP_PROXY = $Url
   $Env:HTTPS_PROXY = $Url
 
+}
+
+function Prompt {
+  $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+  $principal = [Security.Principal.WindowsPrincipal] $identity
+  $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
+  $prefix = @()
+
+  if (Test-Path variable:/PSDebugContext) { $prefix += 'DBG' }
+  if ($principal.IsInRole($adminRole)) { $prefix += 'ADMIN' }
+  if ((Test-Path Env:HTTP_PROYX) -or (Test-Path Env:HTTPS_PROXY)) { $prefix += 'PROXY' }
+
+  $PMT_PREFIX = if ($prefix.Count -eq 0) { '' } else { '[' + ($prefix -join '/') + ']: ' }
+
+  $PMT_PREFIX + 'PS ' + $(Get-Location) +
+    $(if ($NestedPromptLevel -ge 1) { '>>' }) + '> '
 }
 
 $NVIM_HOME = "$HOME\Apps\nvim-win64\Neovim"
