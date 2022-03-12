2c39203cb77f8dc34a70d82180f38cd07a43e0dd
Jonas Gao <gaopinsong@live.cn>
2022-03-12 12:24:31 +0800

    Update Copy-SSHID.ps1

--- powershell/Copy-SSHID.ps1
+++ powershell/Copy-SSHID.ps1
@@ -1,1 +1,8 @@
-type $env:USERPROFILE\.ssh\id_rsa.pub | ssh {IP-ADDRESS-OR-FQDN} "cat >> .ssh/authorized_keys"
+# How to usage
+# `Copy-SSHID user@1.1.1.1`
+# Or
+# `Copy-SSHID user@1.1.1.1 .\.ssh\other_id_rsa.pub`
+$keyPath = $args[2]
+if (!$keyPath) { $keyPath = "$env:USERPROFILE\.ssh\id_rsa.pub" }
+Write-Output "Will copy '$keyPath'"
+Get-Content $keyPath | ssh $args[0] "mkdir -p .ssh; chmod 700 .ssh; cat >> .ssh/authorized_keys"
