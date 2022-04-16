744e1c93bb0868ae8e1f9b23bb87fd860e5d2c8d
JonasGao <gaopinsong@live.cn>
2022-04-16 16:20:20 +0800

    Add Set-XXXHome functions

--- powershell/module/MyPsScripts/MyPsScripts.psm1
+++ powershell/module/MyPsScripts/MyPsScripts.psm1
@@ -15,20 +15,125 @@
   Copy-Sshid some_one@10.0.0.2 .ssh\id_rsa_other.pub
 #>
 function Copy-Sshid {
-    param(
-      [parameter(Mandatory=$true)]
-      [ValidateNotNullOrEmpty()]
-      [string] 
-      $Target, 
-      [string]
-      $KeyFile = ""
-    )
-    if (!$KeyFile) { $KeyFile = "$env:USERPROFILE\.ssh\id_rsa.pub" }
-    if (!$KeyFile.endsWith(".pub")) { $KeyFile = "$KeyFile.pub" }
-    if (!(Test-Path -Path $KeyFile -PathType Leaf)) {
-      throw "'$KeyFile' file is not exists!"
-    }
-    Write-Output "Will copy '$KeyFile' to '$Target'"
-    Get-Content $keyFile | ssh $Target "mkdir -p .ssh; chmod 700 .ssh; cat >> .ssh/authorized_keys"
+  param(
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string] 
+    $Target, 
+    [string]
+    $KeyFile = ""
+  )
+  if (!$KeyFile) { $KeyFile = "$env:USERPROFILE\.ssh\id_rsa.pub" }
+  if (!$KeyFile.endsWith(".pub")) { $KeyFile = "$KeyFile.pub" }
+  if (!(Test-Path -Path $KeyFile -PathType Leaf)) {
+    throw "'$KeyFile' file is not exists!"
   }
-  Export-ModuleMember -Function Copy-Sshid
+  Write-Output "Will copy '$KeyFile' to '$Target'"
+  Get-Content $keyFile | ssh $Target "mkdir -p .ssh; chmod 700 .ssh; cat >> .ssh/authorized_keys"
+}
+
+<#
+ .Synopsis
+  Set current env 'JAVA_HOME' and setup PATH.
+ 
+ .Parameter Path
+  Path to java home.
+
+ .Example
+  Set-JavaHome C:\xxx\java\jdk-1.8
+#>
+function Set-JavaHome {
+
+  param (
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Path
+  )
+  
+  Write-Output "Set JAVA_HOME = '$Path'"
+  $JAVA_HOME = $Path
+  $env:JAVA_HOME = $Path
+  $env:PATH = "$JAVA_HOME\bin;$env:PATH"
+}
+
+<#
+ .Synopsis
+  Setup SBT_HOMe
+ 
+ .Parameter Path
+  Path to sbt home.
+#>
+function Set-SbtHome {
+
+  param (
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Path
+  )
+  
+  Write-Output "Set SBT_HOME = '$Path'"
+  $SBT_HOME = $Path
+  $env:SBT_HOME = $Path
+  $env:PATH = "$SBT_HOME\bin;$env:PATH"
+}
+
+<#
+ .Synopsis
+  Setup MAVEN_HOME
+ 
+ .Parameter Path
+  Path to maven home.
+#>
+function Set-MvnHome {
+
+  param (
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Path
+  )
+  
+  Write-Output "Set MAVEN_HOME = '$Path'"
+  $MAVEN_HOME = $Path
+  $env:MAVEN_HOME = $Path
+  $env:PATH = "$MAVEN_HOME\bin;$env:PATH"
+}
+
+<#
+ .Synopsis
+  Setup http proxy env
+#>
+function Set-HttpProxy ([string]$Url, [switch]$Reset) {
+
+  if ($Reset) {
+    if (Test-Path Env:HTTP_PROXY) { Remove-Item -Path Env:HTTP_PROXY }
+    if (Test-Path Env:HTTPS_PROXY) { Remove-Item -Path Env:HTTPS_PROXY }
+    Write-Output "已清除代理配置"
+    return
+  }
+
+  if (!$Url) {
+    $CONF_FILE = "$HOME\.config\my-powershell\default-proxy"
+    if (Test-Path -Path $CONF_FILE) {
+      $Url = Get-Content $CONF_FILE
+    }
+  }
+
+  if (!$Url) {
+    Write-Output "没有指定 Url 参数，或者提供一个有效的 default-proxy 配置文件"
+    return
+  }
+
+  Write-Output "使用代理：$Url"
+  $Env:HTTP_PROXY = $Url
+  $Env:HTTPS_PROXY = $Url
+
+}
+
+Export-ModuleMember -Function Copy-Sshid
+Export-ModuleMember -Function Set-JavaHome
+Export-ModuleMember -Function Set-SbtHome
+Export-ModuleMember -Function Set-HttpProxy
+Export-ModuleMember -Function Set-MvnHome
--- powershell/profiles/Microsoft.PowerShell_profile.ps1
+++ powershell/profiles/Microsoft.PowerShell_profile.ps1
@@ -5,44 +5,13 @@ Import-Module posh-git
 Import-Module Terminal-Icons
 Import-Module ZLocation
 
+$Env:LESSCHARSET = 'utf-8'
+
+Set-Alias -Name vim -Value gvim
+
+
+
 if (Test-Path "C:\Users\Administrator\.jabba\jabba.ps1") { . "C:\Users\Administrator\.jabba\jabba.ps1" }
-
-function Set-JavaHome ([string]$Path){
-
-  $ABS_PATH = Resolve-Path $Path
-  Write-Output "Set JAVA_HOME = '$ABS_PATH'"
-  $JAVA_HOME = $ABS_PATH
-  $Env:JAVA_HOME = $JAVA_HOME
-  $Env:PATH = "$JAVA_HOME\bin;$env:PATH"
-
-}
-
-function Set-HttpProxy ([string]$Url, [switch]$Reset) {
-
-  if ($Reset) {
-    if (Test-Path Env:HTTP_PROXY) { Remove-Item -Path Env:HTTP_PROXY }
-    if (Test-Path Env:HTTPS_PROXY) { Remove-Item -Path Env:HTTPS_PROXY }
-    Write-Output "已清除代理配置"
-    return
-  }
-
-  if (!$Url) {
-    $CONF_FILE = "$HOME\.config\my-powershell\default-proxy"
-    if (Test-Path -Path $CONF_FILE) {
-      $Url = Get-Content $CONF_FILE
-    }
-  }
-
-  if (!$Url) {
-    Write-Output "没有指定 Url 参数，或者提供一个有效的 default-proxy 配置文件"
-    return
-  }
-
-  Write-Output "使用代理：$Url"
-  $Env:HTTP_PROXY = $Url
-  $Env:HTTPS_PROXY = $Url
-
-}
 
 function Prompt {
   $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
