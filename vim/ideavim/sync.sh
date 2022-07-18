a4958df40efa7cce8733e2f4b3b37503259a17d4
Jonas <gaopinsong@live.cn>
2022-07-18 11:07:20 +0800

    Sync ideavimrc

---
+++ vim/ideavim/sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp "$HOME/.ideavimrc" "$HOME/my_github/my-configs/vim/ideavim/user.vim"
+cp "$HOME/.local/bin/sync-ideavimrc" "$HOME/my_github/my-configs/vim/ideavim/sync.sh"
+cd "$HOME/my_github/my-configs"
+git add vim
+git commit -m 'Sync ideavimrc'
+git push
+printf "\033[0;32mSync ideavimrc finished\033[0m\n"
