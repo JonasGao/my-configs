cb5c3e99c80809249c1d3264a614a830f183a168
Jonas <gaopinsong@live.cn>
2022-08-10 15:21:43 +0800

    Update script-2

--- shell-scripts/jar-control/script-2.sh
+++ shell-scripts/jar-control/script-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APP_NAME="xxxxxxxxxxxxxxx"
+APP_NAME="xxxxx"
 
 # 应用启动的端口
 APP_PORT=17083
@@ -21,13 +21,13 @@ ACTION=$1
 APP_START_TIMEOUT=20
 
 # 应用健康检查URL
-HEALTH_CHECK_URL=http://127.0.0.1:${APP_PORT}
+HEALTH_CHECK_URL="http://127.0.0.1:${APP_PORT}"
 
 # 应用启动的工作目录
 APP_HOME="/usr/local/${APP_NAME}"
 
 # JAR 包的绝对路径
-JAR_NAME=${APP_HOME}/${APP_NAME}.jar # jar包的名字
+JAR_PATH=${APP_HOME}/${APP_NAME}.jar # jar包的名字
 
 # 应用的控制台输出
 # 例如 JAVA_OUT=${APP_HOME}/logs/start.log
@@ -43,95 +43,97 @@ APP_LOG=${JAVA_OUT}
 mkdir -p ${APP_HOME}
 mkdir -p ${APP_LOG_HOME}
 
+# 准备相关工具
+JAVA=$(which java)
+NOHUP=$(which nohup)
+
 usage() {
-    echo "Usage: $PROG_NAME {start|stop|restart}"
+  echo "Usage: $PROG_NAME {start|stop|restart}"
 }
 
 health_check() {
-    exptime=0
-    echo "checking ${HEALTH_CHECK_URL}"
-    while true
-        do
-            status_code=$(/usr/bin/curl -L -o /dev/null --connect-timeout 5 -s -w "%{http_code}" ${HEALTH_CHECK_URL})
-            if [ "$?" != "0" ]; then
-               echo "application not started"
-            else
-                echo "code is $status_code"
-                if [ "$status_code" == "200" ] || [ "$status_code" == "404" ] || [ "$status_code" == "405" ];then
-                    break
-                fi
-            fi
-            sleep 1
-            ((exptime++))
+  exp_time=0
+  echo "Checking ${HEALTH_CHECK_URL}"
+  while true; do
+    if status_code=$(/usr/bin/curl -L -o /dev/null --connect-timeout 5 -s -w "%{http_code}" ${HEALTH_CHECK_URL}); then
+      echo "Status-code is $status_code"
+      if [ "$status_code" == "200" ] || [ "$status_code" == "404" ] || [ "$status_code" == "405" ]; then
+        break
+      fi
+    else
+      echo "Application not started"
+    fi
+    sleep 1
+    ((exp_time++))
 
-            echo "wait app to pass health check: $exptime..."
+    echo "Waiting to health check: $exp_time..."
 
-            if [ $exptime -gt ${APP_START_TIMEOUT} ]; then
-                echo 'app start failed. try tail application log'
-                tail ${APP_LOG}
-               exit 1
-            fi
-        done
-    echo "check ${HEALTH_CHECK_URL} success"
+    if [ "$exp_time" -gt ${APP_START_TIMEOUT} ]; then
+      echo 'app start failed. try tail application log'
+      tail ${APP_LOG}
+      exit 1
+    fi
+  done
+  echo "Health check ${HEALTH_CHECK_URL} success"
 }
 
 start_application() {
-    echo "starting java process (${JAR_NAME})"
-    nohup java ${JVM_OPTS} -jar ${JAR_NAME} ${JAR_ARGS} > ${JAVA_OUT} 2>&1 &
-    echo "started java process"
+  echo "Run (${JAR_PATH})"
+  ${NOHUP} ${JAVA} ${JVM_OPTS} -jar ${JAR_PATH} ${JAR_ARGS} >${JAVA_OUT} 2>&1 &
+  echo "Run jar succeed"
 }
 
 query_java_pid() {
-   ps -ef | grep java | grep ${1} | grep -v grep | grep -v 'deploy.sh' | awk '{print$2}'
+  ps -ef | grep java | grep "${1}" | grep -v grep | grep -v 'deploy.sh' | awk '{print$2}'
 }
 
 stop_application() {
-   checkjavapid=`query_java_pid ${APP_NAME}`
+  check_java_pid=$(query_java_pid ${APP_NAME})
 
-   if [[ ! $checkjavapid ]];then
-      echo "no java process"
-      return
-   fi
+  if [[ ! $check_java_pid ]]; then
+    echo "No java process!"
+    return
+  fi
 
-   echo "stop java process"
-   times=60
-   for e in $(seq 60)
-   do
-        sleep 1
-        COSTTIME=$(($times - $e ))
-        checkjavapid=`query_java_pid ${APP_NAME}`
-        if [[ $checkjavapid ]];then
-            kill -9 $checkjavapid
-            echo -e "        -- stopping java lasts `expr $COSTTIME` seconds."
-        else
-            echo -e "java process has exited"
-            break;
-        fi
-   done
+  echo "Stopping java process"
+  times=60
+  for e in $(seq 60); do
+    sleep 1
+    COST_TIME=$((times - e))
+    check_java_pid=$(query_java_pid ${APP_NAME})
+    if [[ $check_java_pid ]]; then
+      kill -9 "$check_java_pid"
+      echo -e "        -- stopping java lasts ${COST_TIME} seconds."
+    else
+      echo -e "Java process has exited"
+      break
+    fi
+  done
 }
 
 start() {
-    start_application
-    health_check
+  start_application
+  health_check
 }
 
 stop() {
-    stop_application
+  stop_application
 }
 
 case "$ACTION" in
-    start)
-        start
-    ;;
-    stop)
-        stop
-    ;;
-    restart)
-        stop
-        start
-    ;;
-    *)
-        usage
-        exit 1
-    ;;
+start)
+  start
+  ;;
+stop)
+  stop
+  ;;
+restart)
+  stop
+  start
+  ;;
+*)
+  usage
+  exit 1
+  ;;
 esac
+
