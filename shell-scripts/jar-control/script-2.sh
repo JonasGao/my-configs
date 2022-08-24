6572e349a12f79105efc47c7b1c53328f16ef328
Jonas <gaopinsong@live.cn>
2022-08-24 20:40:20 +0800

    Update script-2.sh

--- shell-scripts/jar-control/script-2.sh
+++ shell-scripts/jar-control/script-2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-APP_NAME="xxxxx"
+APP_NAME="eureka"
 
 # 应用启动的端口
-APP_PORT=17083
+APP_PORT=7761
 
 # JVM 配置参数
-JVM_OPTS="-ea -server"
+JVM_OPTS="-server -Xmx512m"
 
 # JAR 包启动的时候传递的参数
-JAR_ARGS="--server.port=${APP_PORT}"
+JAR_ARGS=""
 
 # 当前脚本的名字
 PROG_NAME=$0
@@ -24,20 +24,23 @@ APP_START_TIMEOUT=20
 HEALTH_CHECK_URL="http://127.0.0.1:${APP_PORT}"
 
 # 应用启动的工作目录
-APP_HOME="/usr/local/${APP_NAME}"
+APP_HOME="/home/deployer/retail-cloud/eureka"
 
 # JAR 包的绝对路径
-JAR_PATH=${APP_HOME}/${APP_NAME}.jar # jar包的名字
+JAR_PATH="${APP_HOME}/eureka-server.jar"
 
 # 应用的控制台输出
-# 例如 JAVA_OUT=${APP_HOME}/logs/start.log
-JAVA_OUT="${APP_HOME}/app.log"
+# 例如 STD_OUT=${APP_HOME}/logs/start.log
+STD_OUT="${APP_HOME}/app.log"
 
 # 应用的日志输出路径
 APP_LOG_HOME=${APP_HOME}
 
 # 应用的日志文件
-APP_LOG=${JAVA_OUT}
+APP_LOG=${STD_OUT}
+
+# PID 位置
+PID="${APP_HOME}/pid"
 
 # 创建出相关目录
 mkdir -p ${APP_HOME}
@@ -46,9 +49,14 @@ mkdir -p ${APP_LOG_HOME}
 # 准备相关工具
 JAVA=$(which java)
 NOHUP=$(which nohup)
+PGREP=$(which pgrep1 2>/dev/null)
+
+# 全局变量
+CURR_PID=
+OTHER_RUNNING=false
 
 usage() {
-  echo "Usage: $PROG_NAME {start|stop|restart}"
+  echo "Usage: $PROG_NAME {start|stop|restart|pid}"
 }
 
 health_check() {
@@ -71,24 +79,55 @@ health_check() {
     if [ "$exp_time" -gt ${APP_START_TIMEOUT} ]; then
       echo 'app start failed. try tail application log'
       tail ${APP_LOG}
-      exit 1
+      exit 2
     fi
   done
   echo "Health check ${HEALTH_CHECK_URL} success"
 }
 
 start_application() {
-  echo "Run (${JAR_PATH})"
-  ${NOHUP} ${JAVA} ${JVM_OPTS} -jar ${JAR_PATH} ${JAR_ARGS} >${JAVA_OUT} 2>&1 &
-  echo "Run jar succeed"
+  query_java_pid
+  if [ "$CURR_PID" = "" ]
+  then
+    echo "Run (${JAR_PATH})"
+    ${NOHUP} ${JAVA} ${JVM_OPTS} -jar ${JAR_PATH} ${JAR_ARGS} >${STD_OUT} 2>&1 &
+    pid=$!
+    rc=$?
+    if [ "$rc" = "0" ]; then
+      echo "Run jar succeed ($pid)"
+      echo "$pid" > $PID
+    else
+      echo "Run jar failed with ($rc)"
+      exit 3
+    fi
+  else
+    echo "Running in $CURR_PID"
+    OTHER_RUNNING=true
+  fi
 }
 
 query_java_pid() {
-  ps -ef | grep java | grep "${1}" | grep -v grep | grep -v 'deploy.sh' | awk '{print$2}'
+  if [ -f "$PID" ]; then
+    pid=$(cat "$PID")
+    if ps -p $PID > /dev/null
+    then
+      CURR_PID="$pid"
+    fi
+  fi
+  if [ "$CURR_PID" = "" ]
+  then
+    target=${APP_NAME}
+    if [ "$PGREP" = "" ]
+    then
+      CURR_PID=$(ps -ef | grep java | grep "${target}" | grep -v grep | grep -v "$$" | awk '{print$2}')
+    else
+      CURR_PID=$(${PGREP} -lf "${target}" | grep -v "$$")
+    fi
+  fi
 }
 
 stop_application() {
-  check_java_pid=$(query_java_pid ${APP_NAME})
+  check_java_pid=$(query_java_pid)
 
   if [[ ! $check_java_pid ]]; then
     echo "No java process!"
@@ -100,7 +139,7 @@ stop_application() {
   for e in $(seq 60); do
     sleep 1
     COST_TIME=$((times - e))
-    check_java_pid=$(query_java_pid ${APP_NAME})
+    check_java_pid=$(query_java_pid)
     if [[ $check_java_pid ]]; then
       kill -9 "$check_java_pid"
       echo -e "        -- stopping java lasts ${COST_TIME} seconds."
@@ -113,7 +152,10 @@ stop_application() {
 
 start() {
   start_application
-  health_check
+  if [ ! $OTHER_RUNNING ]
+  then
+    health_check
+  fi
 }
 
 stop() {
@@ -130,6 +172,10 @@ stop)
 restart)
   stop
   start
+  ;;
+pid)
+  query_java_pid
+  echo "Current running in $CURR_PID"
   ;;
 *)
   usage
