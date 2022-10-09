a937c664291b7c30dac8e2f38c1ee5cc751be1e2
Jonas Gao <gaopinsong@live.cn>
2022-10-09 22:04:17 +0800

    Update git setup script

---
+++ git/alias.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.cm 'commit -m'
+git config --global alias.ca 'commit -a'
+git config --global alias.cam 'commit -a -m'
+git config --global alias.st status
+git config --global alias.pt 'push --tags'
+git config --global alias.pr 'pull -r'
+
