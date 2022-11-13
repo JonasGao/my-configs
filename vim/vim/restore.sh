26d708d5c4beee238fc4d055fb0c1a2673902492
Jonas <gaopinsong@live.cn>
2022-11-13 12:19:02 +0800

    Update vim vimrc restore script

--- vim/vim/restore.sh
+++ vim/vim/restore.sh
@@ -3,12 +3,26 @@ if [ -z "$MY_CONFIG_HOME" ]; then
 	echo "NO MY_CONFIG_HOME"
 	exit 1
 fi
-cp "$MY_CONFIG_HOME/vim/vim/vimrc" "$HOME/.vimrc"
-printf "\033[0;32mRestore vimrc finished\033[0m\n"
 
-if read -q REPLY\?"Press [y] install airline: "; then
+if which vim >/dev/null; then
+  xvim="vim"
+elif which vimx >/dev/null; then
+  xvim="vimx"
+else
+  echo "There is no vim"
+  exit 1
+fi
+
+$xvim -d "$MY_CONFIG_HOME/vim/vim/vimrc" "$HOME/.vimrc"
+
+if read -qs REPLY\?"Press [y] restore vimrc"; then
+  cp "$MY_CONFIG_HOME/vim/vim/vimrc" "$HOME/.vimrc"
+  printf "\n\033[0;32mRestore vimrc finished\033[0m\n"
+fi
+
+if echo && read -qs REPLY\?"Press [y] install airline: "; then
 	mkdir -p "$HOME/.vim/pack/dist/start/"
 	git clone git@github.com:vim-airline/vim-airline.git "$HOME/.vim/pack/dist/start/vim-airline"
-elif read -q REPLY\?"Press [y] install custom statusline: "; then
+elif echo && read -qs REPLY\?"Press [y] install custom statusline: "; then
 	cp "$MY_CONFIG_HOME/vim/vim/statusline.vim" "$HOME/.vim/autoload/"
 fi
