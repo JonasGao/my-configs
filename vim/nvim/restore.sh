ba1133cc4cc648ece96e0cebd699c0e58b365dcc
Jonas <gaopinsong@live.cn>
2022-11-13 13:14:58 +0800

    Update neovim restore script

--- vim/nvim/restore.sh
+++ vim/nvim/restore.sh
@@ -7,3 +7,8 @@ mkdir -p "$HOME/.config/nvim/"
 cp "$MY_CONFIG_HOME/vim/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 cp "$MY_CONFIG_HOME/vim/nvim/backup.sh" "$HOME/.local/bin/backup-my-nvimrc"
 printf "\033[0;32mRestore neovim config files finished.\033[0m\n"
+
+if echo && read -qs REPLY\?"Press [y] install airline: "; then
+	mkdir -p "$HOME/.config/nvim/pack/dist/start/"
+	git clone git@github.com:vim-airline/vim-airline.git "$HOME/.config/nvim/pack/dist/start/vim-airline"
+fi
