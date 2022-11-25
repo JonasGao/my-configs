be15e2327e1a2b78cc6b8399328ac01e2595759d
Jonas <gaopinsong@live.cn>
2022-11-25 14:58:34 +0800

    Update zsh(zplug) install_backup.sh

--- zsh/zplug/install_backup.sh
+++ zsh/zplug/install_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 TARGET="$HOME/.local/bin"
 if [ -d "$TARGET" ]; then
-	install backup.sh "$TARGET/backup-my-zplug-zshrc"
+	install backup.sh "$TARGET/backup-my-zshrc-zplug"
 else
 	echo "There is non \"$TARGET\""
 	exit 1
