7e9c8d6a5dc4bfc074fad83c338111f84960c6c7
Jonas <gaopinsong@live.cn>
2022-11-26 10:28:49 +0800

    Backup neovim config

--- vim/nvim/restore.sh
+++ vim/nvim/restore.sh
@@ -5,7 +5,6 @@ if [ -z "$MY_CONFIG_HOME" ]; then
 fi
 mkdir -p "$HOME/.config/nvim/"
 cp "$MY_CONFIG_HOME/vim/nvim/init.vim" "$HOME/.config/nvim/init.vim"
-cp "$MY_CONFIG_HOME/vim/nvim/backup.sh" "$HOME/.local/bin/backup-my-nvimrc"
 printf "\033[0;32mRestore neovim config files finished.\033[0m\n"
 
 if echo && read -qs REPLY\?"Press [y] install airline: "; then
