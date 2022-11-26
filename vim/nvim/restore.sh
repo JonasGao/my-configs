8d965a74c446ce8dffc134894767b0b746521e02
Jonas <gaopinsong@live.cn>
2022-11-26 10:42:06 +0800

    Backup neovim config

--- vim/nvim/restore.sh
+++ vim/nvim/restore.sh
@@ -1,4 +1,8 @@
 #!/bin/zsh
+
+PACK_HOME="$HOME/.config/nvim/pack"
+PACK_START="$PACK_HOME/dist/start"
+
 if [ -z "$MY_CONFIG_HOME" ]; then
 	echo "NO MY_CONFIG_HOME"
 	exit 1
@@ -8,11 +12,22 @@ cp "$MY_CONFIG_HOME/vim/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 printf "\033[0;32mRestore neovim config files finished.\033[0m\n"
 
 if echo && read -qs REPLY\?"Press [y] install airline: "; then
-	mkdir -p "$HOME/.config/nvim/pack/dist/start/"
-	git clone git@github.com:vim-airline/vim-airline.git "$HOME/.config/nvim/pack/dist/start/vim-airline"
+	mkdir -p "$PACK_START/"
+	git clone git@github.com:vim-airline/vim-airline.git "$PACK_START/vim-airline"
 fi
 
 if echo && read -qs REPLY\?"Press [y] install easymotion: "; then
-	mkdir -p "$HOME/.config/nvim/pack/dist/start/"
-	git clone git@github.com:easymotion/vim-easymotion.git "$HOME/.config/nvim/pack/dist/start/vim-easymotion"
+	mkdir -p "$PACK_START/"
+	git clone git@github.com:easymotion/vim-easymotion.git "$PACK_START/vim-easymotion"
+fi
+
+if echo && read -qs REPLY\?"Press [y] install fzf: "; then
+	mkdir -p "$PACK_START/"
+	git clone git@github.com:junegunn/fzf.git "$PACK_START/fzf"
+	git clone git@github.com:junegunn/fzf.vim.git "$PACK_START/fzf.vim"
+fi
+
+if echo && read -qs REPLY\?"Press [y] install vim-visual-multi: "; then
+	mkdir -p "$PACK_START/"
+  git clone git@github.com:mg979/vim-visual-multi.git "$PACK_START/vim-visual-multi"
 fi
