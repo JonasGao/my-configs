7d56db793ab4cb6e8320092bd7b4e1c41d367db5
Jonas <gaopinsong@live.cn>
2022-11-26 10:36:34 +0800

    Backup vimrc

--- vim/vim/restore.sh
+++ vim/vim/restore.sh
@@ -36,3 +36,8 @@ if echo && read -qs REPLY\?"Press [y] install fzf: "; then
 	git clone git@github.com:junegunn/fzf.git ~/.vim/pack/packages/start/fzf
 	git clone git@github.com:junegunn/fzf.vim.git ~/.vim/pack/packages/start/fzf.vim
 fi
+
+if echo && read -qs REPLY\?"Press [y] install vim-visual-multi: "; then
+	mkdir -p "$HOME/.vim/pack/plugins/start/"
+  git clone git@github.com:mg979/vim-visual-multi.git ~/.vim/pack/packages/start/vim-visual-multi
+fi
