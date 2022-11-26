22579508a38c199cad500c185e993cc9c0982452
Jonas <gaopinsong@live.cn>
2022-11-26 17:33:10 +0800

    Backup ideavimrc

--- vim/ideavim/restore.sh
+++ vim/ideavim/restore.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 if [ -z "$MY_CONFIG_HOME" ]; then
 	echo "NO MY_CONFIG_HOME"
 	exit 1
 fi
-cp "$MY_CONFIG_HOME/vim/ideavim/user.vim" "$HOME/.ideavimrc"
-cp "$MY_CONFIG_HOME/vim/ideavim/backup.sh" "$HOME/.local/bin/backup-my-ideavimrc"
-printf "\033[0;32mRestore ideavimrc finished\033[0m\n"
+SRC="$MY_CONFIG_HOME/vim/ideavim/user.vim"
+DST="$HOME/.ideavimrc"
+vim -d $DST $SRC
+if read -qs REPLY\?"Press [y] restore: "; then
+  echo
+  cp "$SRC" "$DST"
+  printf "\033[0;32mRestore ideavimrc finished\033[0m\n"
+fi
