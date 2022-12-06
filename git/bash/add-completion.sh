4e3245c056bd1b712336a471a62019330b957975
Jonas <gaopinsong@live.cn>
2022-12-06 17:15:42 +0800

    Update add-completion git script

--- git/bash/add-completion.sh
+++ git/bash/add-completion.sh
@@ -1,6 +1,7 @@
-cd ~
+#!/bin/bash
+cd "$HOME"
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 chmod u+x git-completion.bash
 mv git-completion.bash .git-completion.bash
-echo "source ~/.git-completion.bash" >> .bashrc
-source ~/.git-completion.bash
+echo "source $HOME/.git-completion.bash" >> .bashrc
+source "$HOME/.git-completion.bash"
