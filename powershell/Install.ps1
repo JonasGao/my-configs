dacf8fddd46f258bafc5cc582a03137e217e3562
JonasGao <gaopinsong@live.cn>
2022-12-12 20:12:38 +0800

    Update Powershell Install.ps1

--- powershell/Install.ps1
+++ powershell/Install.ps1
@@ -1,3 +1,4 @@
+$USER_MODULE_HOME = $env:PSModulePath.Split(";")[0]
 if (-not(Test-Path Variable:\MY_CONFIG_HOME)) {
 	throw "There is no MY_CONFIG_HOME"
 }
@@ -11,5 +12,14 @@ if (Test-Path $PROFILE) {
 $X = Read-Host -Prompt "Do you want install powershell profile? [y]"
 if ($X -eq "y") {
 	Copy-Item $SOURCE $PROFILE
+	Install-Module -Name DotNetVersionLister -Scope CurrentUser
+	if (-not(Test-Path $USER_MODULE_HOME)) {
+		New-Item $USER_MODULE_HOME -Force
+	}
+	Copy-Item -Recurse "module\MyPsScripts" $USER_MODULE_HOME
+	Install-Module -Name MyPsScripts -Scope CurrentUser
+	Install-Module -Name posh-git -Scope CurrentUser
+	Install-Module -Name Terminal-Icons -Scope CurrentUser
+	Install-Module -Name ZLocation -Scope CurrentUser
 	Write-Host "Success install profile." -ForegroundColor Green
 }
