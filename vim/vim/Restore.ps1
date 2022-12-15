415f61976a04551eb38d29552dc8cbb6916fdd2d
JonasGao <gaopinsong@live.cn>
2022-12-15 11:52:17 +0800

    Update vim Restore.ps1

--- vim/vim/Restore.ps1
+++ vim/vim/Restore.ps1
@@ -1,3 +1,7 @@
+$VIM_CONF_HOME="$HOME/vimfiles"
+$PACK_HOME="$VIM_CONF_HOME/pack"
+$PACK_START="$PACK_HOME/packages/start"
+
 if (-not(Test-Path Variable:\MY_CONFIG_HOME)) {
   throw "Can not found MY_CONFIG_HOME"
 }
@@ -10,24 +14,28 @@ if ($REPLY -eq "y") {
   Write-Host -ForegroundColor Green "Restore vimrc finished."
 }
 
+function Install-Plugin {
+  param(
+    $Name,
+    $Repo
+  )
+  $REPLY = Read-Host -Prompt "Press [y] install `"$Name`""
+  if ($REPLY -eq "y") {
+  	New-Item -Force -Type Container "$PACK_START/" > $null
+  	git clone "git@github.com:$Repo.git" "$PACK_START/$Name"
+  } 
+}
+
+Install-Plugin -Name airline -Repo vim-airline/vim-airline
+Install-Plugin -Name easymotion -Repo easymotion/vim-easymotion
+Install-Plugin -Name fzf -Repo junegunn/fzf
+Install-Plugin -Name fzf.vim -Repo junegunn/fzf.vim
+Install-Plugin -Name vim-visual-multi -Repo mg979/vim-visual-multi
+
 #if echo && read -qs REPLY\?"Press [y] install airline: "; then
 #	mkdir -p "$HOME/.vim/pack/dist/start/"
 #	git clone git@github.com:vim-airline/vim-airline.git "$HOME/.vim/pack/dist/start/vim-airline"
 #elif echo && read -qs REPLY\?"Press [y] install custom statusline: "; then
 #	cp "$MY_CONFIG_HOME/vim/vim/statusline.vim" "$HOME/.vim/autoload/"
 #fi
-#
-#if echo && read -qs REPLY\?"Press [y] install easymotion: "; then
-#	mkdir -p "$HOME/.vim/pack/plugins/start/"
-#	git clone git@github.com:easymotion/vim-easymotion.git ~/.vim/pack/plugins/start/vim-easymotion
-#fi
-#
-#if echo && read -qs REPLY\?"Press [y] install fzf: "; then
-#	git clone git@github.com:junegunn/fzf.git ~/.vim/pack/packages/start/fzf
-#	git clone git@github.com:junegunn/fzf.vim.git ~/.vim/pack/packages/start/fzf.vim
-#fi
-#
-#if echo && read -qs REPLY\?"Press [y] install vim-visual-multi: "; then
-#	mkdir -p "$HOME/.vim/pack/plugins/start/"
-#  git clone git@github.com:mg979/vim-visual-multi.git ~/.vim/pack/packages/start/vim-visual-multi
-#fi
+
