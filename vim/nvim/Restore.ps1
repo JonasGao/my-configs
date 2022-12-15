3fdb595d83c6f05c926b8f8ac6974acc533fc468
JonasGao <gaopinsong@live.cn>
2022-12-15 11:46:41 +0800

    Update nvim Restore.ps1

--- vim/nvim/Restore.ps1
+++ vim/nvim/Restore.ps1
@@ -9,23 +9,21 @@ New-Item -Type Container -Force "$NVIM_CONF_HOME"
 Copy-Item "$MY_CONFIG_HOME/vim/nvim/init.vim" "$NVIM_CONF_HOME/init.vim"
 Write-Host -ForegroundColor Green "Restore neovim config files finished."
 
-#if echo && read -qs REPLY\?"Press [y] install airline: "; then
-#	mkdir -p "$PACK_START/"
-#	git clone git@github.com:vim-airline/vim-airline.git "$PACK_START/vim-airline"
-#fi
-#
-#if echo && read -qs REPLY\?"Press [y] install easymotion: "; then
-#	mkdir -p "$PACK_START/"
-#	git clone git@github.com:easymotion/vim-easymotion.git "$PACK_START/vim-easymotion"
-#fi
-#
-#if echo && read -qs REPLY\?"Press [y] install fzf: "; then
-#	mkdir -p "$PACK_START/"
-#	git clone git@github.com:junegunn/fzf.git "$PACK_START/fzf"
-#	git clone git@github.com:junegunn/fzf.vim.git "$PACK_START/fzf.vim"
-#fi
-#
-#if echo && read -qs REPLY\?"Press [y] install vim-visual-multi: "; then
-#	mkdir -p "$PACK_START/"
-#  git clone git@github.com:mg979/vim-visual-multi.git "$PACK_START/vim-visual-multi"
-#fi
+function Install-Plugin {
+  param(
+    $Name,
+    $Repo
+  )
+  $REPLY = Read-Host -Prompt "Press [y] install `"$Name`""
+  if ($REPLY -eq "y") {
+  	New-Item -Force -Type Container "$PACK_START/"
+  	git clone "git@github.com:$Repo.git" "$PACK_START/$Name"
+  } 
+}
+
+Install-Plugin -Name airline -Repo vim-airline/vim-airline
+Install-Plugin -Name easymotion -Repo easymotion/vim-easymotion
+Install-Plugin -Name fzf -Repo junegunn/fzf
+Install-Plugin -Name fzf.vim -Repo junegunn/fzf.vim
+Install-Plugin -Name vim-visual-multi -Repo mg979/vim-visual-multi
+
