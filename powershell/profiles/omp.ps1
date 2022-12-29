5f96414274524ff9b43aacdca51ae8a5a684b45c
JonasGao <gaopinsong@live.cn>
2022-12-29 15:59:33 +0800

    Update powershell omp profile

--- powershell/profiles/omp.ps1
+++ powershell/profiles/omp.ps1
@@ -5,9 +5,11 @@ Import-Module MyPsScripts
 Import-Module Terminal-Icons
 Import-Module ZLocation
 
-oh-my-posh init pwsh --config "$HOME\.mytheme.omp.json" | Invoke-Expression
-
 Set-Alias -Name vim -Value gvim
+Set-Alias -Name ll -Value Get-ChildItem
 
 $Env:LESSCHARSET = 'utf-8'
+
 if (Test-Path "$HOME/Set-Env.ps1") { . "$HOME/Set-Env.ps1" }
+
+oh-my-posh init pwsh --config $PoshConfig | Invoke-Expression
