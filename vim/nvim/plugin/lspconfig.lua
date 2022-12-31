484fa62988950857fe2ebcb3c9ee6ce441e9fe16
Jonas Gao <gaopinsong@live.cn>
2022-12-31 23:43:10 +0800

    Update nvim lspconfig

--- vim/nvim/plugin/lspconfig.lua
+++ vim/nvim/plugin/lspconfig.lua
@@ -2,7 +2,20 @@ local lspconfig = require'lspconfig'
 
 require('vim.lsp.protocol')
 
+local on_attach = function(client, bufnr)
+  -- formatting
+  -- this will be update reference project 'lspconfig'
+  if client.server_capabilities.documentFormattingProvider then
+    local api = vim.api
+    api.nvim_command [[augroup Format]]
+    api.nvim_command [[autocmd! * <buffer>]]
+    api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+    api.nvim_command [[augroup END]]
+  end
+end
+
 lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
   settings = {
     Lua = {
       runtime = {
@@ -28,7 +41,8 @@ lspconfig.sumneko_lua.setup {
 local pses_path = vim.fn.expand('$HOME/PowerShellEditorServices')
 
 lspconfig.powershell_es.setup {
-  bundle_path=pses_path
+  on_attach = on_attach,
+  bundle_path=pses_path,
 }
 
 vim.diagnostic.config({
