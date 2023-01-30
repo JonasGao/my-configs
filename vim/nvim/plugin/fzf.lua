f0d2f9e62a9401cbe458addf5844260f87eb555a
JonasGao <gaopinsong@live.cn>
2023-01-30 19:39:04 +0800

    Fix neovim fzf keymap

--- vim/nvim/plugin/fzf.lua
+++ vim/nvim/plugin/fzf.lua
@@ -1,5 +1,5 @@
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>ff', ':Files', opts)
-vim.keymap.set('n', '<space>fg', ':GFiles', opts)
-vim.keymap.set('n', '<space>fa', ':Ag', opts)
-vim.keymap.set('n', '<space>fr', ':Rg', opts)
+vim.keymap.set('n', '<space>ff', ':Files<CR>', opts)
+vim.keymap.set('n', '<space>fg', ':GFiles<CR>', opts)
+vim.keymap.set('n', '<space>fa', ':Ag<CR>', opts)
+vim.keymap.set('n', '<space>fr', ':Rg<CR>', opts)
