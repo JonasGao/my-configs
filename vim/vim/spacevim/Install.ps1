6931ccb3b1189d8d02d4a4fa0b9d1b52837ab6b6
JonasGao <gaopinsong@live.cn>
2023-02-02 11:09:43 +0800

    Move spacevim out, and add more lang layers

--- vim/vim/spacevim/Install.ps1
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/SpaceVim/SpaceVim.git "$HOME\.SpaceVim"
-New-Item -Path "$HOME\vimfiles" -ItemType Junction -Target "$HOME\.SpaceVim"
-Copy-Item -Recuse "$MY_CONFIG_HOME\vim\vim\spacevim\.SpaceVim.d" "$HOME\"
