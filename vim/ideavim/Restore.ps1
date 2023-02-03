74f7b91169cb1895501dd8067d064c4acb3d70e2
JonasGao <gaopinsong@live.cn>
2023-02-03 11:19:35 +0800

    Update ideavim

--- vim/ideavim/Restore.ps1
+++ vim/ideavim/Restore.ps1
@@ -1,11 +1,25 @@
-if (-not(Test-Path Variable:\MY_CONFIG_HOME)) {
-  throw "There is no MY_CONFIG_HOME"
+param(
+  [switch]$Install,
+  [switch]$Backup
+)
+if (-not(Test-Path Variable:\MY_CONFIG_HOME))
+{
+  Write-Host "There is no MY_CONFIG_HOME" -ForegroundColor Red
+  Exit 1
 }
-$SRC = "$MY_CONFIG_HOME/vim/ideavim/user.vim"
-$DST = "$HOME/.ideavimrc"
-nvim -d $DST $SRC
-$REPLY = Read-Host -Prompt "Press [y] restore ideavim"
+if ($Install)
+{
+  $SRC = "$MY_CONFIG_HOME/vim/ideavim/.ideavimrc"
+  $DST = "$HOME/.ideavimrc"
+}
+if ($Backup)
+{
+  $DST = "$MY_CONFIG_HOME/vim/ideavim/.ideavimrc"
+  $SRC = "$HOME/.ideavimrc"
+}
+delta $DST $SRC
+$REPLY = Read-Host -Prompt "Press [y] continue..."
 if ($REPLY -eq "y") {
   Copy-Item "$SRC" "$DST"
-  Write-Host -ForegroundColor Green "Restore ideavimrc finished."
+  Write-Host -ForegroundColor Green "Finished."
 }
