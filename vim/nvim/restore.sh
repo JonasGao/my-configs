f7cd5452b2eb0285cf5ee046d52a1092166a3294
Jonas Gao <gaopinsong@live.cn>
2023-02-11 21:54:49 +0800

    Update neovim restore.sh

--- vim/nvim/restore.sh
+++ vim/nvim/restore.sh
@@ -1,45 +1,52 @@
-#!/bin/zsh
+#!/bin/bash
 
 NVIM_CONF_HOME="$HOME/.config/nvim"
 PACK_HOME="$NVIM_CONF_HOME/pack"
 PACK_START="$PACK_HOME/dist/start"
+SCRIPT_NAME="$0"
 
 if [ -z "$MY_CONFIG_HOME" ]
 then
-	echo "NO MY_CONFIG_HOME"
-	exit 1
+  echo "NO MY_CONFIG_HOME"
+  exit 1
 fi
 
 mkdir -p "$HOME/.config/nvim/"
 
-rercfile() {
-  SRC="$MY_CONFIG_HOME/vim/nvim/init.vim"
-  DST="$HOME/.config/nvim/init.vim"
-  nvim -d $DST $SRC
-  cp $SRC $DST
-  printf "\033[0;32mRestore neovim config files finished.\033[0m\n"
+usage() {
+  echo "Usage: $SCRIPT_NAME [-arpch]" >&2
+  exit 2
 }
 
-install-packer() {
+set_variable() {
+  local varname=$1
+  shift
+  if [ -z "${!varname}" ]; then
+    eval "$varname=\"$@\""
+  else
+    echo "Error: $varname already set" >&2
+    exit 3
+  fi
+}
+
+restore_rcfile() {
+  SRC="$MY_CONFIG_HOME/vim/nvim/init.vim"
+  DST="$HOME/.config/nvim/init.vim"
+  cp "$SRC" "$DST"
+  printf "\033[0;32mRestore from %s to %s.\033[0m\n" "$SRC" "$DST"
+}
+
+install_packer() {
   D="$PACK_START/packer.nvim"
   if [ -d "$D" ]
   then
-    exit 2
+    printf "\033[0;33mPacker already exists.\033[0m\n"
+    exit 4
   fi
-  git clone https://github.com/wbthomason/packer.nvim $D
+  git clone https://github.com/wbthomason/packer.nvim "$D"
 }
 
-cloneplg() {
-  NAME=$1
-  REPO=$2
-  if echo && read -qs REPLY\?"Press [y] install \"$NAME\": "
-  then
-    mkdir -p "$PACK_START/"
-    git clone "git@github.com:$REPO.git" "$PACK_START/$NAME"
-  fi
-}
-
-reconf() {
+restore_conf() {
   N="$MY_CONFIG_HOME/vim/nvim"
   L="$N/lua"
   P="$N/plugin"
@@ -47,62 +54,37 @@ reconf() {
   cp -R $L "$NVIM_CONF_HOME/"
   cp -R $P "$NVIM_CONF_HOME/"
   cp -R $F "$NVIM_CONF_HOME/"
+  printf "\033[0;32mRestore config files.\033[0m\n"
 }
 
-while getopts "arpck" OPT
+install_dependencies() {
+  # For telescope-fzf-native
+  scoop install cmake
+  # For treesitter
+  scoop install gcc
+  # For lsp
+  scoop install lua-language-server
+  # Maybe for telescope preview
+  scoop install bat
+
+  Build-TelescopeFzfNative
+}
+
+###########################
+# Main script starts here
+
+unset ACTION
+while getopts "arpc?h" OPT
 do
   case "$OPT" in
-    a)
-      ALL=1
-      ;;
-    r)
-      INIT=1
-      ;;
-    p)
-      PLUGIN=1
-      ;;
-    c)
-      CONFIG=1
-      ;;
-    k)
-      PACKER=1
-      ;;
-    ?)
-      echo "arpck" >&2
-      exit 3
-      ;;
+    a) set_variable ACTION all ;;
+    r) set_variable ACTION restore_rcfile ;;
+    c) set_variable ACTION restore_conf ;;
+    p) set_variable ACTION install_packer ;;
+    h|?) usage ;;
   esac
 done
 
-if [ "$ALL" = "1" ]
-then
-  INIT=1
-  PLUGIN=1
-  PACKER=1
-  CONFIG=1
-fi
+[ -z "$ACTION" ] && usage
 
-if [ "$INIT" = "1" ]
-then
-  rercfile
-fi
-
-if [ "$PACKER" = "1" ]
-then
-  install-packer
-fi
-
-if [ "$PLUGIN" = "1" ]
-then
-  # Replaced by lualine
-  # Install-Plugin -Name airline vim-airline/vim-airline
-  cloneplg easymotion easymotion/vim-easymotion
-  cloneplg fzf junegunn/fzf
-  cloneplg fzf.vim junegunn/fzf.vim
-  cloneplg vim-visual-multi mg979/vim-visual-multi
-fi
-
-if [ "$CONFIG" = "1" ]
-then
-  reconf
-fi
+eval "$ACTION"
