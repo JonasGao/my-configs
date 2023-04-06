1f12a3ef7dd7ea9bfab7ab81dbf96f64c8b9c379
JonasGao <gaopinsong@live.cn>
2023-04-06 12:07:51 +0800

    Update powershell profile, add alias vim to nvim

--- powershell/profiles/Set-Env.ps1
+++ powershell/profiles/Set-Env.ps1
@@ -16,18 +16,24 @@ $PoshThemeConfig = "$($env:LOCALAPPDATA)\Programs\oh-my-posh\themes\$PoshTheme.o
 $MyPoshThemeConfig = "$HOME\.my.$PoshTheme.omp.json"
 $PoshConfig = $MyPoshThemeConfig
 
-function sortpom {
+Set-Alias -Name "vim" -Value "nvim"
+
+function sortpom
+{
   param (
     $Settings
   )
-  if ($Settings) {
+  if ($Settings)
+  {
     Write-Output "Using: $Settings"
     mvn -s $Settings com.github.ekryd.sortpom:sortpom-maven-plugin:sort "-Dsort.encoding=UTF-8" "-Dsort.keepBlankLines=false" "-Dsort.sortDependencies=optional,scope,groupId,artifactId" "-Dsort.sortPlugins=groupId,artifactId" "-Dsort.sortProperties=true" "-Dsort.sortExecutions=true" "-Dsort.sortDependencyExclusions=groupId,artifactId" "-Dsort.lineSeparator=`"\n`"" "-Dsort.ignoreLineSeparators=false" "-Dsort.expandEmptyElements=false" "-Dsort.nrOfIndentSpace=2" "-Dsort.indentSchemaLocation=true"
-  } else {
+  } else
+  {
     mvn com.github.ekryd.sortpom:sortpom-maven-plugin:sort "-Dsort.encoding=UTF-8" "-Dsort.keepBlankLines=false" "-Dsort.sortDependencies=optional,scope,groupId,artifactId" "-Dsort.sortPlugins=groupId,artifactId" "-Dsort.sortProperties=true" "-Dsort.sortExecutions=true" "-Dsort.sortDependencyExclusions=groupId,artifactId" "-Dsort.lineSeparator=`"\n`"" "-Dsort.ignoreLineSeparators=false" "-Dsort.expandEmptyElements=false" "-Dsort.nrOfIndentSpace=2" "-Dsort.indentSchemaLocation=true"
   }
 }
 
-function cumv {
+function cumv
+{
   mvn -s $CUM2 $args
 }
