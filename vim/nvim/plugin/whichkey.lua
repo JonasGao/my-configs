7c5daa69c8eaf264c22cf78b192f21b37a57a8f3
JonasGao <gaopinsong@live.cn>
2023-04-09 14:23:08 +0800

    Update neovim which-key settings, add custom mapping labels

--- vim/nvim/plugin/whichkey.lua
+++ vim/nvim/plugin/whichkey.lua
@@ -5,6 +5,7 @@ vim.o.timeout = true
 vim.o.timeoutlen = 500
 whichkey.setup {
   plugins = {
+    marks = true,
     registers = true,
     presets = {
       motions = true,
@@ -13,7 +14,7 @@ whichkey.setup {
       text_objects = true,
     },
   },
-  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " },
+  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^call ", "^lua ", "^:", "^ " },
   triggers_blacklist = {
     -- list of mode / prefixes that should never be hooked by WhichKey
     -- this is mostly relevant for key maps that start with a native binding
@@ -21,6 +22,8 @@ whichkey.setup {
     i = { "j", "k" },
     v = { "j", "k" },
   },
+  show_help = true,
+  show_keys = true,
   -- disable the WhichKey popup for certain buf types and file types.
   -- Disabled by deafult for Telescope
   disable = {
@@ -28,3 +31,19 @@ whichkey.setup {
     filetypes = { "TelescopePrompt" },
   },
 }
+
+whichkey.register({
+  f = {
+    name = "Fuzzy Finder",
+    f = { "<cmd>Telescope find_files<cr>", "find_files" },
+    g = { "<cmd>Telescope live_grep<cr>", "live_grep" },
+    b = { "<cmd>Telescope buffers<cr>", "buffers" },
+    h = { "<cmd>Telescope help_tags<cr>", "help_tags" },
+    v = { "<cmd>Telescope git_files<cr>", "git_files" },
+    e = { "<cmd>Telescope oldfiles<cr>", "oldfiles" },
+    c = { "<cmd>Telescope commands<cr>", "commands" },
+    H = { "<cmd>Telescope command_history<cr>", "command_history" },
+    r = { "<cmd>Telescope registers<cr>", "registers" },
+    C = { "<cmd>Telescope colorscheme<cr>", "colorscheme" },
+  }
+}, { prefix = "<leader>" })
