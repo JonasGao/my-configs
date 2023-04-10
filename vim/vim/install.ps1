e5fa7e04b5e685624d7c11233d728f8ace36a9c2
JonasGao <gaopinsong@live.cn>
2023-04-10 11:44:12 +0800

    Remove vim conf, move it to independent repo

--- vim/vim/install.ps1
+++ vim/vim/install.ps1
@@ -1,21 +1,10 @@
 param (
   [Switch]$Plug,
   [Switch]$Conf,
-  [Switch]$Clean,
   $Proxy
 )
 
-if (-not(Test-Path -Path "$MY_CONFIG_HOME" -PathType Container))
-{
-  $ErrorMsg="There is no MY_CONFIG_HOME(Value:$MY_CONFIG_HOME)"
-  throw $ErrorMsg
-}
-
 $VimConfHome="$HOME/vimfiles"
-$PackHome="$VimConfHome/pack"
-$PackStart="$PackHome/packages/start"
-$ConfSrc = "$MY_CONFIG_HOME/vim/vim/plug"
-$AutoloadHome = "$VimConfHome/autoload"
 
 function Resolve-Dir
 {
@@ -28,10 +17,9 @@ function Resolve-Dir
   }
 }
 
-Resolve-Dir $PackStart
-
 function InstallPlug
 {
+  $AutoloadHome = "$VimConfHome/autoload"
   Write-Output "Installing plug-vim"
   Resolve-Dir $AutoloadHome
   try
@@ -53,8 +41,7 @@ function InstallPlug
 
 function RestoreConf
 {
-  Copy-Item "$ConfSrc/.vimrc" "$HOME/.vimrc"
-  Copy-Item "$ConfSrc/vimfiles/plugin" "$HOME/vimfiles/plugin" -Recurse
+  git clone "git@github.com:jonasgao/vimfiles.git" $VimConfHome
   Write-Host "`e[32mAlready overwrite.`e[0m"
 }
 
@@ -66,18 +53,4 @@ if ($Plug)
 if ($Conf)
 {
   RestoreConf
-}
-
-if ($Clean)
-{
-  Write-Output "Do Cleanup..."
-  if (Test-Path -Path $VimConfHome -PathType Container)
-  {
-    Write-Output "Force remove $VimConfHome"
-    Remove-Item -Force -Recurse "$VimConfHome"
-  } else
-  {
-    Write-Output "Force remove $VimConfHome"
-    Remove-Item -Force "$VimConfHome"
-  }
 }
--- vim/vim/plug/vimfiles/plugin/vim-lsp.vim
+++
@@ -1,3 +0,0 @@
-let g:lsp_log_verbose = 1
-let g:lsp_log_file = expand('~/vimfiles/vim-lsp.log')
-let g:lsp_settings_filetype_javascript = ['javascript-typescript-stdio']
