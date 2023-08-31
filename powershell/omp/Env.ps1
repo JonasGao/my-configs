cc251d71543c630c71b764b30e9681913a45ad17
JonasGao <gaopinsong@live.cn>
2023-08-31 16:46:27 +0800

    Update powershell env

--- powershell/omp/Env.ps1
+++ powershell/omp/Env.ps1
@@ -4,10 +4,10 @@ $env:PATH = $env:PATH + ";$HOME\bin;$HOME\.local\bin"
 
 $MAVEN_HOME = "D:\Maven\apache-maven-3.8.6-bin\apache-maven-3.8.6\bin"
 $env:PATH = $env:PATH + ";" + $MAVEN_HOME
-$CUM2 = "$HOME/.m2/_codeup/settings.xml"
-$HSM2 = "$HOME/.m2/_huashang/settings.xml"
+$M2_CU = "$HOME/.m2/_codeup/settings.xml"
+$M2_HS = "$HOME/.m2/_huashang/settings.xml"
 
-$NVIM_HOME = "$HOME\Apps\nvim-win64\Neovim"
+$NVIM_HOME = "$env:NVIM_HOME"
 $NVIM_CONF = "$env:LOCALAPPDATA\nvim"
 $env:PATH = "$NVIM_HOME\bin;$env:PATH"
 
@@ -19,24 +19,49 @@ $PoshConfig = $MyPoshThemeConfig
 Import-Module "$MY_CONFIG_HOME\powershell\module\JdkSwitcher\JdkSwitcher.psm1"
 Import-Module "$MY_CONFIG_HOME\powershell\module\MyPsScripts\MyPsScripts.psm1"
 
-function sortpom
+function Update-Env
+{
+  if (-not (Test-Path -Path Env:\PWSH_PROFILE_HOME))
+  {
+    throw "Not found env:PWSH_PROFILE_HOME!"
+  }
+  . "$env:PWSH_PROFILE_HOME\Env.ps1"
+}
+
+function Format-Pom
 {
   param (
     $Settings
   )
+  $arr = @(
+    "com.github.ekryd.sortpom:sortpom-maven-plugin:sort",
+    "-Dsort.encoding=UTF-8",
+    "-Dsort.keepBlankLines=false",
+    "-Dsort.sortDependencies=optional,scope,groupId,artifactId",
+    "-Dsort.sortPlugins=groupId,artifactId",
+    "-Dsort.sortProperties=true",
+    "-Dsort.sortExecutions=true",
+    "-Dsort.sortDependencyExclusions=groupId,artifactId",
+    "-Dsort.lineSeparator=`"\n`"",
+    "-Dsort.ignoreLineSeparators=false",
+    "-Dsort.expandEmptyElements=false",
+    "-Dsort.nrOfIndentSpace=2",
+    "-Dsort.indentSchemaLocation=true"
+  )
+
   if ($Settings)
   {
     Write-Output "Using: $Settings"
-    mvn -s $Settings com.github.ekryd.sortpom:sortpom-maven-plugin:sort "-Dsort.encoding=UTF-8" "-Dsort.keepBlankLines=false" "-Dsort.sortDependencies=optional,scope,groupId,artifactId" "-Dsort.sortPlugins=groupId,artifactId" "-Dsort.sortProperties=true" "-Dsort.sortExecutions=true" "-Dsort.sortDependencyExclusions=groupId,artifactId" "-Dsort.lineSeparator=`"\n`"" "-Dsort.ignoreLineSeparators=false" "-Dsort.expandEmptyElements=false" "-Dsort.nrOfIndentSpace=2" "-Dsort.indentSchemaLocation=true"
+    mvn -s $Settings $arr
   } else
   {
-    mvn com.github.ekryd.sortpom:sortpom-maven-plugin:sort "-Dsort.encoding=UTF-8" "-Dsort.keepBlankLines=false" "-Dsort.sortDependencies=optional,scope,groupId,artifactId" "-Dsort.sortPlugins=groupId,artifactId" "-Dsort.sortProperties=true" "-Dsort.sortExecutions=true" "-Dsort.sortDependencyExclusions=groupId,artifactId" "-Dsort.lineSeparator=`"\n`"" "-Dsort.ignoreLineSeparators=false" "-Dsort.expandEmptyElements=false" "-Dsort.nrOfIndentSpace=2" "-Dsort.indentSchemaLocation=true"
+    mvn $arr
   }
 }
 
 function cumv
 {
-  mvn -s $CUM2 $args
+  mvn -s $M2_CU $args
 }
 
 function Set-GoPathLocation
