c7cdfad35bcb5d61812f9217de7f0cac02b3b97a
Jonas Gao <gaopinsong@live.cn>
2023-12-02 14:01:12 +0800

    Remove delta installation from ideavim install

--- vim/ideavim/install.ps1
+++ vim/ideavim/install.ps1
@@ -4,7 +4,6 @@ if (-not(Test-Path Variable:\MY_CONFIG_HOME))
   Exit 1
 }
 $USER_MODULE_HOME = $env:PSModulePath.Split(";")[0]
-scoop install delta
 New-Item "$USER_MODULE_HOME\IdeavimScripts" -Type Directory -ErrorAction Ignore | Out-Null
 Copy-Item "$MY_CONFIG_HOME\vim\ideavim\IdeavimScripts.psm1" "$USER_MODULE_HOME\IdeavimScripts\"
 Write-Output "Installed module IdeavimScripts"
