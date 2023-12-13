912d11081bf30086d6b08416a5aef8f1e7965e6f
Jonas Gao <gaopinsong@live.cn>
2023-12-13 10:58:56 +0800

    Update PowerShell Env

--- powershell/omp/Env.ps1
+++ powershell/omp/Env.ps1
@@ -1,13 +1,10 @@
-$MY_CONFIG_HOME = "$HOME\my-github\my-configs"
-
-$MAVEN_VERSION = "3.9.4"
-$MAVEN_HOME = "D:\Maven\apache-maven-$MAVEN_VERSION-bin\apache-maven-$MAVEN_VERSION\bin"
-
-$M2_CU = "$HOME/.m2/_codeup/settings.xml"
-$M2_HS = "$HOME/.m2/_huashang/settings.xml"
-
-$POSH_HOME = "C:\Users\udemo\scoop\apps\oh-my-posh\current\"
+$env:MY_CONFIG_HOME = "$HOME\Github\jonas\my-configs"
+$env:MAVEN_VER = "3.8.7"
+$env:MAVEN_HOME = "D:\Maven\apache-maven-$MAVEN_VER\bin"
+$env:M2_CU = "$HOME/.m2/_codeup/settings.xml"
+$env:M2_HS = "$HOME/.m2/_huashang/settings.xml"
+$env:POSH_HOME = "C:\Users\udemo\scoop\apps\oh-my-posh\current\"
 $PoshTheme = "amro"
 $PoshThemeConfig = "$POSH_HOME\themes\$PoshTheme.omp.json"
 $MyPoshThemeConfig = "$HOME\.my.$PoshTheme.omp.json"
-$PoshConfig = $PoshThemeConfig
+$env:POSH_CONFIG = $PoshThemeConfig
--- powershell/omp/Microsoft.PowerShell_profile.ps1
+++ powershell/omp/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,14 @@
-$PROFILE_HOME = (Get-Item $PROFILE).Directory
-$ENV_FILENAME = "Env.ps1"
-$ENV_FILE = "$PROFILE_HOME/$ENV_FILENAME"
-$NVIM_CONF = "$env:LOCALAPPDATA\nvim"
-$NVIM_HOME = "$env:NVIM_HOME"
+$env:PROFILE_HOME = (Get-Item $PROFILE).Directory
+$env:ENV_FILENAME = "Env.ps1"
+$env:ENV_FILE = "$env:PROFILE_HOME/$env:ENV_FILENAME"
+$env:NVIM_CONF = "$env:LOCALAPPDATA\nvim"
+$env:NVIM_HOME = "$env:NVIM_HOME"
 
 function Update-Env
 {
-  if (Test-Path "$ENV_FILE")
+  if (Test-Path "$env:ENV_FILE")
   {
-    . "$ENV_FILE"
+    . "$env:ENV_FILE"
   }
 }
 
@@ -26,12 +26,11 @@ $env:POSH_GIT_ENABLED = $true
 Update-Env
 
 $env:PATH = "$HOME\bin;$HOME\.local\bin;" + $env:PATH
-$env:PATH = "$MAVEN_HOME;" + $env:PATH
-$env:PATH = "$NVIM_HOME\bin;$env:PATH"
+$env:PATH = "$env:MAVEN_HOME;" + $env:PATH
+$env:PATH = "$env:NVIM_HOME\bin;$env:PATH"
 
 Import-Module posh-git
 Import-Module Terminal-Icons
-Import-Module z
-Import-Module "$MY_CONFIG_HOME\powershell\module\MyPsScripts"
+Import-Module "$env:MY_CONFIG_HOME\powershell\module\MyPsScripts"
 
-oh-my-posh init pwsh --config $PoshConfig | Invoke-Expression
+oh-my-posh init pwsh --config $env:POSH_CONFIG | Invoke-Expression
