0fc10ed13dfee55701965fa5d1f4fb34ba99eaf8
Jonas Gao <gaopinsong@live.cn>
2023-12-13 19:41:18 +0800

    Backup ideavimrc

--- vim/ideavim/IdeavimScripts.psm1
+++ vim/ideavim/IdeavimScripts.psm1
@@ -1,23 +1,23 @@
 function Compare-Ideavimrc()
 {
-  if (-not(Test-Path Variable:\MY_CONFIG_HOME))
+  if (-not(Test-Path Env:\MY_CONFIG_HOME))
   {
     Write-Host "There is no MY_CONFIG_HOME" -ForegroundColor Red
     Exit 1
   }
-  $SRC = "$MY_CONFIG_HOME/vim/ideavim/.ideavimrc"
+  $SRC = "$env:MY_CONFIG_HOME/vim/ideavim/.ideavimrc"
   $DST = "$HOME/.ideavimrc"
   delta $DST $SRC
 }
 
 function Update-Ideavimrc()
 {
-  if (-not(Test-Path Variable:\MY_CONFIG_HOME))
+  if (-not(Test-Path Env:\MY_CONFIG_HOME))
   {
     Write-Host "There is no MY_CONFIG_HOME" -ForegroundColor Red
     Exit 1
   }
-  $SRC = "$MY_CONFIG_HOME/vim/ideavim/.ideavimrc"
+  $SRC = "$env:MY_CONFIG_HOME/vim/ideavim/.ideavimrc"
   $DST = "$HOME/.ideavimrc"
   delta $DST $SRC
   $REPLY = Read-Host -Prompt "Press [y] continue..."
@@ -30,12 +30,12 @@ function Update-Ideavimrc()
 
 function Backup-Ideavimrc()
 {
-  if (-not(Test-Path Variable:\MY_CONFIG_HOME))
+  if (-not(Test-Path Env:\MY_CONFIG_HOME))
   {
     Write-Host "There is no MY_CONFIG_HOME" -ForegroundColor Red
     Exit 1
   }
-  $DST = "$MY_CONFIG_HOME/vim/ideavim/.ideavimrc"
+  $DST = "$env:MY_CONFIG_HOME/vim/ideavim/.ideavimrc"
   $SRC = "$HOME/.ideavimrc"
   delta $DST $SRC
   $REPLY = Read-Host -Prompt "Press [y] continue..."
@@ -43,7 +43,7 @@ function Backup-Ideavimrc()
   {
     Copy-Item "$SRC" "$DST"
     Write-Host -ForegroundColor Green "Finished."
-    Push-Location "$MY_CONFIG_HOME/vim/ideavim"
+    Push-Location "$env:MY_CONFIG_HOME/vim/ideavim"
     git add .
     git commit -m "Backup ideavimrc"
     git push
--- vim/ideavim/install.ps1
+++ vim/ideavim/install.ps1
@@ -1,4 +1,4 @@
-if (-not(Test-Path Variable:\MY_CONFIG_HOME))
+if (-not(Test-Path env:\MY_CONFIG_HOME))
 {
   Write-Host "There is no MY_CONFIG_HOME" -ForegroundColor Red
   Exit 1
@@ -8,5 +8,5 @@ Write-Output "Installed dependency: delta"
 $USER_MODULE_HOME = $env:PSModulePath.Split(";")[0]
 Write-Output "Will install IdeavimScripts to `"$USER_MODULE_HOME`""
 New-Item "$USER_MODULE_HOME\IdeavimScripts" -Type Directory -ErrorAction Ignore | Out-Null
-Copy-Item "$MY_CONFIG_HOME\vim\ideavim\IdeavimScripts.psm1" "$USER_MODULE_HOME\IdeavimScripts\"
+Copy-Item "$env:MY_CONFIG_HOME\vim\ideavim\IdeavimScripts.psm1" "$USER_MODULE_HOME\IdeavimScripts\"
 Write-Output "Installed module IdeavimScripts. Please restart PWSH and run `"Update-Ideavimrc`""
