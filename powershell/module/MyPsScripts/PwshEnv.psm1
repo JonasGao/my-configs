b02d51f45ef41f0d76ed5f0e752bb2facebeae92
JonasGao <gaopinsong@live.cn>
2023-12-19 15:54:01 +0800

    Add Save-Profile

--- powershell/module/MyPsScripts/PwshEnv.psm1
+++ powershell/module/MyPsScripts/PwshEnv.psm1
@@ -1,8 +1,8 @@
 function Update-EnvFile
 {
-  if ((Test-Path Variable:\MY_CONFIG_HOME) -and (Test-Path Variable:\PWSH_PROFILE_HOME))
+  if ((Test-Path Env:\MY_CONFIG_HOME) -and (Test-Path Env:\PROFILE_HOME))
   {
-    Copy-Item -Force -Confirm "$MY_CONFIG_HOME\powershell\omp\Env.ps1" "$PWSH_PROFILE_HOME\Env.ps1"
+    Copy-Item -Force -Confirm "$env:MY_CONFIG_HOME\powershell\omp\Env.ps1" "$env:PROFILE_HOME\Env.ps1"
     Write-Host -ForegroundColor Green "Updated Env.ps1 file."
   } else
   {
@@ -12,11 +12,11 @@ function Update-EnvFile
 
 function Backup-EnvFile
 {
-  if ((Test-Path Variable:\MY_CONFIG_HOME) -and (Test-Path Variable:\PWSH_PROFILE_HOME))
+  if ((Test-Path Env:\MY_CONFIG_HOME) -and (Test-Path Env:\PROFILE_HOME))
   {
-    Copy-Item -Force "$PWSH_PROFILE_HOME\Env.ps1" "$MY_CONFIG_HOME\powershell\omp\Env.ps1"
+    Copy-Item -Force "$env:PROFILE_HOME\Env.ps1" "$env:MY_CONFIG_HOME\powershell\omp\Env.ps1"
     Write-Host -ForegroundColor Green "Backup Env.ps1 file."
-    Push-Location $MY_CONFIG_HOME
+    Push-Location $env:MY_CONFIG_HOME
     git diff
     git add .
     git commit
@@ -28,5 +28,25 @@ function Backup-EnvFile
   }
 }
 
+function Save-Profile
+{
+  if ((Test-Path Env:\MY_CONFIG_HOME))
+  {
+    $TARGET_DIR = "$env:MY_CONFIG_HOME\powershell\omp\"
+    Copy-Item -Force "$PROFILE" "$TARGET_DIR"
+    Write-Host -ForegroundColor Green "Copied $PROFILE"
+    Push-Location $TARGET_DIR
+    git diff
+    git add .
+    git commit
+    git push
+    Pop-Location
+  } else
+  {
+    Write-Host -ForegroundColor Red "Failed Save. Missing Env:MY_CONFIG_HOME."
+  }
+}
+
 Export-ModuleMember -Function Update-EnvFile
 Export-ModuleMember -Function Backup-EnvFile
+Export-ModuleMember -Function Save-Profile
