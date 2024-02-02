b2a033e29e7334a66dc45eca6d15c72854ed8daf
Jonas Gao <gaopinsong@live.cn>
2024-02-02 20:46:21 +0800

    Update MvnSwitcher

--- powershell/module/MyPsScripts/MvnSwitcher.psm1
+++ powershell/module/MyPsScripts/MvnSwitcher.psm1
@@ -1,10 +1,12 @@
-$MVN_LIST_FILE = "$HOME\.mvn\mvn_install.csv"
+$CONFIG_HOME = "$HOME\.mvn"
+$MVN_LIST_FILE = "$CONFIG_HOME\mvn_install.csv"
+$LATEST_USE_MVN = "$CONFIG_HOME\latest_use"
 
 function Read-Data()
 {
   if (-not (Test-Path -Path $MVN_LIST_FILE -PathType Leaf))
   {
-    New-Item $MVN_LIST_FILE > $null
+    New-Item -Force $MVN_LIST_FILE > $null
   }
   $csv = Import-Csv $MVN_LIST_FILE
   $table = @{}
@@ -22,23 +24,22 @@ function Save-Data()
   $Table.GetEnumerator() | Select-Object -Property Key,Value | Export-Csv -NoTypeInformation -Path $MVN_LIST_FILE
 }
 
-function Set-MvnHome()
+function Add-MvnHome()
 {
   param (
-    [parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     [ValidateScript({ !$_.Contains(":") })]
-    [string]
-    $Key,
-    [parameter(Mandatory = $true)]
+    [string] $Key,
+    [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     [ValidateScript({ Test-Path -Path $_ -PathType container })]
-    [string]
-    $Path
+    [string] $Path
   )
   $dic = Read-Data
-  $fullName = (Get-Item $Path).FullName
-  $dic[$Key] = $fullName
+  $full = (Get-Item $Path).FullName
+  Write-Host -ForegroundColor Green "Add maven [$Key] -> `"$full`""
+  $dic[$Key] = $full
   Save-Data $dic
 }
 
@@ -50,64 +51,75 @@ function Get-MvnHome()
 function Search-MvnHome()
 {
   param (
-    [parameter]
     [ValidateNotNullOrEmpty()]
     [ValidateScript({ Test-Path -Path $_ -PathType container })]
-    [string]
-    $Path = ".",
-    [parameter]
-    [int]
-    $Depth = 3
+    [string] $Path = ".",
+    [int] $Depth = 3,
+    [Switch] $Save
   )
   $items = Get-ChildItem -Path $Path -Depth $Depth -Directory -ErrorAction Ignore
   $items = $items | Where-Object { $_.Name -eq "bin"}
-  $items = $items | Where-Object { Test-Path ($_.FullName + "\mvn.exe") -PathType Leaf }
+  $items = $items | Where-Object { Test-Path (Join-Path $_.FullName "\mvn.cmd") }
   $items | ForEach-Object {
-    Write-Output $_.Parent.FullName
+    if ($Save)
+    {
+      Add-MvnHome -Key $_.Parent.Name -Path $_.Parent.FullName
+    } else
+    {
+      Write-Output $_.Parent.FullName
+    }
+  }
+}
+
+function latestuse()
+{
+  param([Switch]$Set, $Value)
+  if ($Set)
+  {
+    New-Item -Force $LATEST_USE_MVN -ItemType File > $null
+    Set-Content -Path $LATEST_USE_MVN -Value $Value
+  } else
+  {
+    Get-Content $LATEST_USE_MVN
   }
 }
 
 function setupenv ()
 {
   $target_path = $args[0]
-  $latest_path = $env:JAVA_HOME
-  Write-Output "Set JAVA_HOME = '$target_path'"
-  $JAVA_HOME = $target_path
-  $env:JAVA_HOME = $target_path
+  $latest_path = $env:MAVEN_HOME
+  $env:MAVEN_HOME = $target_path
   if ($latest_path)
   {
+    if (-not (Test-Path Env:\ORIGIN_MAVEN_HOME))
+    {
+      $env:ORIGIN_MAVEN_HOME = $latest_path
+    }
     $new_path = $env:PATH.Replace(($latest_path + "\bin;"), "")
-    $env:PATH = "$JAVA_HOME\bin;$new_path"
+    $env:PATH = "$target_path\bin;$new_path"
   } else
   {
-    $env:PATH = "$JAVA_HOME\bin;$env:PATH"
+    $env:PATH = "$target_path\bin;$env:PATH"
   }
+  latestuse -Set -Value $target_path
 }
 
-<#
-.Synopsis
-Set current env 'JAVA_HOME' and setup PATH.
-
-.Parameter Path
-Path to java home.
-
-.Example
-Use-JavaHome C:\xxx\java\jdk-1.8
-#>
 function Use-Mvn
 {
   param (
     [string]
-    $Path,
+    $Key,
     [string]
-    $Key
+    $Path
   )
   if ($Key)
   {
+    Write-Host -ForegroundColor Green "Find maven by key: $Key"
     $dic = Read-Data
     $target = $dic[$Key]
     if ($target -and (Test-Path -Path $target -PathType container))
     {
+      Write-Host -ForegroundColor Green "Set MAVEN_HOME = '$target_path'"
       setupenv $target
     } else
     {
@@ -118,6 +130,7 @@ function Use-Mvn
 
   if ($Path)
   {
+    Write-Host -ForegroundColor Green "Set MAVEN_HOME = '$target_path'"
     setupenv $Path
     return
   }
@@ -148,7 +161,9 @@ function Install-Mvn()
   Write-Output "Expanded"
 }
 
-Export-ModuleMember -Function Set-MvnHome
+setupenv (latestuse)
+
+Export-ModuleMember -Function Add-MvnHome
 Export-ModuleMember -Function Get-MvnHome
 Export-ModuleMember -Function Use-Mvn
 Export-ModuleMember -Function Search-MvnHome
