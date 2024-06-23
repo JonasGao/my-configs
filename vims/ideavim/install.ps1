c359d195a4ed2b55d23cb259ba5ffcf63b271add
JonasGao <gaopinsong@live.cn>
2024-06-23 13:15:21 +0800

    Fix ideavim install scripts

--- vims/ideavim/install.ps1
+++ vims/ideavim/install.ps1
@@ -8,5 +8,5 @@ Write-Output "Installed dependency: delta"
 $USER_MODULE_HOME = $env:PSModulePath.Split(";")[0]
 Write-Output "Will install IdeavimScripts to `"$USER_MODULE_HOME`""
 New-Item "$USER_MODULE_HOME\IdeavimScripts" -Type Directory -ErrorAction Ignore | Out-Null
-Copy-Item "$env:MY_CONFIG_HOME\vim\ideavim\IdeavimScripts.psm1" "$USER_MODULE_HOME\IdeavimScripts\"
+Copy-Item "$env:MY_CONFIG_HOME\vims\ideavim\IdeavimScripts.psm1" "$USER_MODULE_HOME\IdeavimScripts\"
 Write-Output "Installed module IdeavimScripts. Please restart PWSH and run `"Update-Ideavimrc`""
