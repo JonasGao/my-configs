be6e0bd39ff9fe75fd31b70dbd4066e9b6a6f752
Jonas Gao <gaopinsong@live.cn>
2024-10-20 10:43:31 +0800

    Update install.ps1

--- git/install.ps1
+++ git/install.ps1
@@ -1,3 +1,8 @@
-scoop install delta
+if (Get-Command scoop -errorAction SilentlyContinue)
+{
+    scoop install delta
+} else {
+    Write-Output "Not found scoop, skip install delta."
+}
 Copy-Item "$MY_CONFIG_HOME\git\.gitconfig" "$HOME\.gitconfig" -Force
 Write-Output "Installed .gitconfig."
