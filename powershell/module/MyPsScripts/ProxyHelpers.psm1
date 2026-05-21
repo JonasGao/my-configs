231538070f274ac4c3e56298ed17f39b82844781
JonasGao <gaopinsong@live.cn>
2026-05-21 14:51:26 +0800

    feat: Add Invoke-WithProxy function

--- powershell/module/MyPsScripts/ProxyHelpers.psm1
+++ powershell/module/MyPsScripts/ProxyHelpers.psm1
@@ -73,7 +73,67 @@ function Get-EnvProxy
   return $null
 }
 
+<#
+ .Synopsis
+  Run command with http proxy env temporarily set
+#>
+function Invoke-WithProxy {
+  param (
+    [string]$Url,
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string]$Command,
+    [Parameter(ValueFromRemainingArguments = $true)]
+    $Arguments
+  )
+
+  if (!$Url)
+  {
+    $CONF_FILE = "$HOME\.config\my-powershell\default-proxy"
+    if (Test-Path -Path $CONF_FILE)
+    {
+      $Url = Get-Content $CONF_FILE
+    }
+  }
+
+  if (!$Url)
+  {
+    Write-Error "没有指定 Url 参数，或者提供一个有效的 default-proxy 配置文件"
+    return
+  }
+
+  $oldHttpProxy = if (Test-Path Env:HTTP_PROXY) { $Env:HTTP_PROXY } else { $null }
+  $oldHttpsProxy = if (Test-Path Env:HTTPS_PROXY) { $Env:HTTPS_PROXY } else { $null }
+
+  try
+  {
+    $Env:HTTP_PROXY = $Url
+    $Env:HTTPS_PROXY = $Url
+    & $Command @Arguments
+  }
+  finally
+  {
+    if ($null -eq $oldHttpProxy)
+    {
+      Remove-Item -Path Env:HTTP_PROXY -ErrorAction SilentlyContinue
+    }
+    else
+    {
+      $Env:HTTP_PROXY = $oldHttpProxy
+    }
+
+    if ($null -eq $oldHttpsProxy)
+    {
+      Remove-Item -Path Env:HTTPS_PROXY -ErrorAction SilentlyContinue
+    }
+    else
+    {
+      $Env:HTTPS_PROXY = $oldHttpsProxy
+    }
+  }
+}
+
 Export-ModuleMember -Function Set-EnvProxy
 Export-ModuleMember -Function Get-EnvProxy
 Export-ModuleMember -Function Set-DotNetProxy
 Export-ModuleMember -Function Get-DotNetProxy
+Export-ModuleMember -Function Invoke-WithProxy
